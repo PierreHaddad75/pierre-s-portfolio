a1c4e32b6bbd8d91c607f1bfaa8d1f7687295b83
PierreHaddad75 <166314106+PierreHaddad75@users.noreply.github.com>
2024-04-07 18:16:46 +0300

    Add files via upload

---
+++ assets/js/meyawo.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+
+function openTab() {
+    var name = document.getElementById("name").value;
+    var text = document.getElementById("comment").value;
+    
+    window.open("https://wa.me/+9613340963?text=name: "+ name+". "+ text+"." , "_blank");
+}
